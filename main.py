55605489ea3c56b7dd4890b9e788aa0d8f1fbcd4
RobinSuthar <152766032+RobinSuthar@users.noreply.github.com>
2024-11-15 15:29:57 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],  
+    allow_origins=["https://highlannderaibot.netlify.app"],  
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"],  
